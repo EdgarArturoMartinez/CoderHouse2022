3396e794dac63c5747d428db5befa14ec9c2ef2e
Edgar Arturo Martínez <edgar.arturo.martinez@gmail.com>
2022-07-13 17:55:47 -0500

    Commit1 With new project with OOP concepts C# .Net core 6

---
+++ CoderHouse/ConceptosPOO/ConceptosPOO/Program.cs
@@ -0,0 +1,27 @@
+﻿//********* UTILIZAR PRIVATE ES ENCAPSULAR
+//********* STATIC ES PARA METODO U OBJETO QUE NO NECESITA APLICAR POO
+//********* STATIC NO NECESITA SER INSTANCIADO COMO UN OBJETO, SOLO NOMBRE CLASE Y NOMBRE METODO
+//********* EN LA HERENCIA ESTA INCLUIDO EL POLIMORFISMO (RUNTIME POLYIMORPHISM USING VIRTUAL AND OVERRIDE)
+//********* C# NO ES MULTIHERENCIA.
+//********* DEFAULT CONSTRUCTOR ES AQUEL QUE NO TIENE CODIGO O PARAMETROS (CUANDO LA CLASE NO TIENE CONSTRUCTOR)
+//********* PRIVATE STATIC INT PARA ADMINISTRAR VALORES DE CONTADORES EN UNA CLASE COMO BUENA PRACTICA
+
+using ConceptosPOO;
+
+realizarTarea();
+
+
+static void realizarTarea()
+{
+    Punto origen = new Punto();
+
+    Punto destino = new Punto(150, 90);
+
+    double distancia = origen.DistanciaHasta(destino);
+
+    Console.WriteLine($"Distancia entre puntos es de: {distancia}");
+
+    //Acceso con nombre de clase a metodo Static
+    Console.WriteLine($"Numero de Objetos creados: {Punto.ContadorDeObjetos()}");
+    
+}
---
+++ CoderHouse/ConceptosPOO/ConceptosPOO/Punto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPOO
+{
+    internal class Punto
+    {
+        private int x, y;
+
+        private static int contadorDeObjetos = 0;
+        public Punto()
+        {
+            //Console.WriteLine("Constructor por defecto");
+            this.x = 0;
+            this.y = 0;
+            contadorDeObjetos++;
+        }
+
+        public Punto(int x, int y)
+        {
+            //Console.WriteLine($"Coordenada X: {x}, Coordenada Y: {y}");
+            this.x = x;
+            this.y = y;
+            contadorDeObjetos++;
+        }
+
+        public double DistanciaHasta(Punto otroPunto)
+        {
+            int xDif = this.x - otroPunto.x;
+            int yDif = this.y - otroPunto.y;    
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif,2) + Math.Pow(yDif,2));  
+            return distanciaPuntos;
+        }
+
+        public static int ContadorDeObjetos()
+        {
+            return contadorDeObjetos;
+        }
+    }
+}
