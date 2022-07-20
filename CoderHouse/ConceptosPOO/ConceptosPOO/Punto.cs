aa5b4257694d1f319f422ccb19832fd7cbc3c498
Edgar Arturo Martínez <edgar.arturo.martinez@gmail.com>
2022-07-20 08:06:34 -0500

    Commit to send first delivery on C# coderhouse course.

--- CoderHouse/ConceptosPOO/ConceptosPOO/Punto.cs
+++ CoderHouse/ConceptosPOO/ConceptosPOO/Punto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConceptosPOO
+﻿namespace ConceptosPOO
 {
     internal class Punto
     {
@@ -30,8 +24,8 @@ namespace ConceptosPOO
         public double DistanciaHasta(Punto otroPunto)
         {
             int xDif = this.x - otroPunto.x;
-            int yDif = this.y - otroPunto.y;    
-            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif,2) + Math.Pow(yDif,2));  
+            int yDif = this.y - otroPunto.y;
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif, 2) + Math.Pow(yDif, 2));
             return distanciaPuntos;
         }
 
---
+++ CoderHouse/Desafio1_EdgarMartinez/Desafio1_EdgarMartinez/Program.cs
@@ -0,0 +1,24 @@
+﻿//Calling NameSpace of the solution to allow instance to another Classes.
+using Desafio1_EdgarMartinez;
+
+//Instance each class with an object using default Constructor.
+User objUser = new User();
+Product objProduct = new Product();
+Sales objSales = new Sales();
+SoldProduct objSoldProduct = new SoldProduct();
+
+//Welcome App Section
+Console.WriteLine("**********************************************");
+Console.WriteLine("    Welcome to Coder House C# Application     ");
+
+
+
+
+
+
+
+
+
+
+
+
