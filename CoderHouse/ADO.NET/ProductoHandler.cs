7b7192ddf062be974f197864d966ada6ca1ccc5c
Edgar Arturo Martínez <edgar.arturo.martinez@gmail.com>
2022-08-03 18:30:22 -0500

    Commit ADO.NET practice

---
+++ CoderHouse/ADO.NET/ProductoHandler.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ADO.NET
+{
+    public class ProductoHandler
+    {
+        public const string ConnectionString = 
+            "SERVER=YUXMED0040L;Database=SystemManagement;User Id=sa;Password=80172454.Yuxi;";
+
+        sqlConn
+    }
+}
